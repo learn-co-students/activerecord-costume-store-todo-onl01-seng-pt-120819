8401811f65540ddde297cf435cba472bbe8b0024
github username <github email address>
2020-03-13 14:33:05 -0400

    Done.

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -7,3 +7,20 @@
 # Define a change method in which to do the migration
 # In this change method, create columns with the correct names and 
 # value types according to the spec
+
+class CreateCostumes < ActiveRecord::Migration[5.2]
+    def change
+        create_table :costumes do |t|
+            t.string :name
+            t.integer :price
+            t.string :size
+            t.string :image_url
+            # t.datetime = :created_at
+            # t.datetime = :updated_at
+            t.timestamps null: false
+        end
+
+        # it should have a name, price, size, image url as well as two "timestamp" column: created_at and updated_at. 6 columns
+    end
+
+end
